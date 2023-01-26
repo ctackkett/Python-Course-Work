34794926927dba5214102aed6cea3209f3ef6c13
ctackkett <70414586+ctackkett@users.noreply.github.com>
2023-01-26 16:14:37 -0600

    Delete Unit 2 Assignment 2.py

--- Unit 2 Assignment 2.py
+++
@@ -1,6 +0,0 @@
-height = (int(input("Please enter a height: ")))
-width = (int(input("Please enter a width: ")))
-print("*" * width)
-for x in range(height):
-    print("*" + (" " * (width - 2) + "*"))
-print("*" * width)
